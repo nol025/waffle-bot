3319b3d0ebd7f34b821b053c8693ab0c1c336297
mac user <macuser@MacBook-Air.local>
2025-08-06 19:52:23 +0200

    Fix import for solana Keypair (use solders)

--- woffle_bot_live.py
+++ woffle_bot_live.py
@@ -8,9 +8,10 @@ import base64
 import requests
 from dotenv import load_dotenv
 from solana.rpc.api import Client
-from solana.keypair import Keypair
+from solders.keypair import Keypair
 from solana.transaction import Transaction
 from solana.rpc.types import TxOpts
+from solders.pubkey import Pubkey
 
 # ---------- env ----------
 load_dotenv()
