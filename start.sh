d92785ac0b22012459605f0f1988b13db0092f96
mac user <macuser@MacBook-Air.local>
2025-08-06 19:42:17 +0200

    Fix start.sh: use python -m pip

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-# re-install everything into the buildpack's virtualenv
-pip install --no-cache-dir -r requirements.txt
+# re-install everything into the buildpack’s venv
+python -m pip install --no-cache-dir -r requirements.txt
 
 # hand off to your bot
 exec python woffle_bot_live.py
